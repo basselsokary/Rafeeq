5ce2bd4f4934baefc053cd0de63ed593b99ac3be
basselsokary <basselelsokary@gmail.com>
2026-05-29 22:12:41 +0300

    feat: add additional CORS policy for HTTPS frontend URL

--- backend/Rafeeq/src/API/DependencyInjection.cs
+++ backend/Rafeeq/src/API/DependencyInjection.cs
@@ -117,6 +117,7 @@ public static class DependencyInjection
                 
                 policy.WithOrigins(
                     "http://localhost:5173", // Frontend URL
+                    "https://localhost:5173", // Frontend URL
                     "http://localhost:5143",
                     "https://rafeeq-inky.vercel.app",
                     "https://admin.rafeeq.live"
