ed91aa8e9f5d617035abe03c863fb49e64fb38d7
basselsokary <basselelsokary@gmail.com>
2026-06-09 17:51:03 +0300

    remove some comments

--- web-app/rafeeq-admin/src/main.jsx
+++ web-app/rafeeq-admin/src/main.jsx
@@ -4,10 +4,8 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import { SidebarProvider } from './context/SidebarContext.jsx';
-// import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
         <SidebarProvider>
@@ -15,6 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </SidebarProvider>
       </AuthProvider>
     </ThemeProvider>
-  // </React.StrictMode>
-    // <App />
 );
